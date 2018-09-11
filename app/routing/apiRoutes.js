3b7f1cfda7d1bfbd4d5005b2116472230974831d
Evan Simon Ross <evan.simon.ross@gmail.com>
2018-09-11 00:22:45 -0400

    Added data reading from form

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,10 +1,10 @@
 var friends = require("../data/friends.js");
 
 var routes = function(app){
-    app.get("api/friends", function(req, res){
+    app.get("/api/friends", function(req, res){
         res.json(friends);
     });
-    app.post("api/friends", function(req, res){
+    app.post("/api/friends", function(req, res){
         friends.push(req.body);
         res.json(true);
     });

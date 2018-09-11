e8191dc421ec2efd5c9c27efea4bee87139bdcc3
Evan Simon Ross <evan.simon.ross@gmail.com>
2018-09-11 00:53:34 -0400

    Added functionality to discover best friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -7,11 +7,11 @@ module.exports = [
     {
         name: "Arthur",
         photo: "https://images-na.ssl-images-amazon.com/images/G/01/digital/video/hero/TVSeries/Arthur_3919300-ARUR._V392939155_RI_SX940_.jpg",
-        score: [4,1,1,5,2,3,1,4,1,1]
+        scores: [4,1,1,5,2,3,1,4,1,1]
     },
     {
         name: "Norbert",
         photo: "https://statici.behindthevoiceactors.com/behindthevoiceactors/_img/chars/norbert-foster-beaver-angry-beavers-5.33.jpg",
-        score: [3,2,5,5,1,2,4,1,1,5]
+        scores: [3,2,5,5,1,2,4,1,1,5]
     }
 ];

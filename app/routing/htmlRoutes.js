016062b552a59b30f1cf67142f92b7ac5a7a4574
Evan Simon Ross <evan.simon.ross@gmail.com>
2018-09-11 20:05:06 -0400

    Added other pages

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,9 +1,11 @@
 var path = require("path");
 
 var routes = function(app){
+    app.get("/", function(req, res){
+        return res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
     app.get("/:path", function(req, res){
         switch(req.params.path){
-            case "": return res.sendFile(path.join(__dirname, "../public/home.html"));
             case "survey": return res.sendFile(path.join(__dirname, "../public/survey.html"));
             default: return res.status(404).sendFile(path.join(__dirname,"../public/404.html"));
         }
